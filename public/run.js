f08cf9f53641ce473b3d3fc9c4e05fb907cc8eb8
ueyuki <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-08-07 22:20:20 +0900

    Make run.js for submit and run the code

---
+++ public/run.js
@@ -0,0 +1,26 @@
+function runCode()
+{
+  $("run_button").text("実行中...").prop("disabled", true);
+
+  var language = $("#language").val();
+  var source_code = aceEditor.getValue();
+  var input = $("#input").val();
+  $.ajax({
+    url: "url/run",
+    method: "POST",
+    data: {
+      language: language,
+      source_code: source_code,
+      input: input,
+    },
+  }).done(function(result){
+    $("#stdout").text(result.stdout);
+    $("#stderr").text(result.stderr);
+    $("#time").text(result.time);
+    $("#exit_code").text(result.exit_code);
+    $("#run_button").text("実行(Ctrl-Enter)").prop("disabled", false);
+  }).fail(function(error){
+    alert("Request Failed:" + error);
+    $("#run_button").text("実行(Ctrl-Enter)").prop("disabled", false);
+  });
+}
